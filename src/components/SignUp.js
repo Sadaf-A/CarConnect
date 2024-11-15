47de0ef1251725d5ffe3ab5109803acb73a23439
Sadaf <119438857+Sadaf-A@users.noreply.github.com>
2024-11-15 21:05:47 +0530

    Update SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -12,7 +12,7 @@ const SignUp = () => {
 
   const handleSignUp = async () => {
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/register', {
+      const response = await axios.post('https://global-dominion-383716.el.r.appspot.com/api/auth/register', {
         username,
         email,
         password,
